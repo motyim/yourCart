62ff105f70a32fd2f8c60444167b37326e612732
Mohamed MotYim <mohamed.motyim@gmail.com>
2017-04-21 23:53:20 +0200

    add config file

--- yourCart/src/java/org/yourcart/model/DbConnection.java
+++ yourCart/src/java/org/yourcart/model/DbConnection.java
@@ -1,30 +1,55 @@
 package org.yourcart.model;
 
-
 /**
  *
  * @author OsamaPC
  */
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConnection {
 
-
     Connection con = null;
+    final private String url;
+    final private String user;
+    final private String pass;
 
+    public DbConnection() {
+        
+        Properties prop = new Properties();
+
+        try {
+
+            //access properties file
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream input = classLoader.getResourceAsStream("config.properties");
+
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //get value from Properties file 
+        url = prop.getProperty("url", "");
+        user = prop.getProperty("username", "");
+        pass = prop.getProperty("password", "");
+
+    }
 
     public Connection openConnection() {
         try {
 
             Class.forName("org.mariadb.jdbc.Driver");
-            
+
             //connect to DB
             con = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost/yourCart",
-                    "root",
-                    "");
+                    url,
+                    user,
+                    pass);
 
             System.out.println(con);
             return con;
@@ -48,4 +73,3 @@ public class DbConnection {
         return false;
     }
 }
-
